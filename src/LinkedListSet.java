2a6dfe1b696a86b5fc21b0890fc433cd0e65dc9e
Aahlad <aahladkethineeedi@gmail.com>
2021-07-15 19:27:47 +0530

    changes

--- src/LinkedListSet.java
+++ src/LinkedListSet.java
@@ -1,7 +1,7 @@
 // https://practice.geeksforgeeks.org/problems/deque-implementations/0/?category[]=Java-Collections&category[]=Java-Collections&difficulty[]=0&page=1&query=category[]Java-Collectionsdifficulty[]0page1category[]Java-Collections
 public class LinkedListSet {
     public static void main(String args[]) {
-
+        System.out.println("asd");
     }
 }
 
